7111e5c9c3af58dd22acbbffdf6777a95ecdd85a
sean-slaughter <sthompson2822@gmail.com>
2020-11-22 11:53:45 -0600

    Edit styling on ME page

--- src/containers/MonitoringEvaluation.js
+++ src/containers/MonitoringEvaluation.js
@@ -47,23 +47,23 @@ export default function MonitoringEvaluation() {
                             <h1>Monitoring &amp; Evaluation Steps</h1> 
                             <br/>
                             <p>
-                                <Image className="d-block mx-auto img-fluid w-50" src="/monitoringevaluation.webp" fluid/>
+                                <Image className="d-block mx-auto img-fluid w-50" style={{minWidth: 300}} src="/monitoringevaluation.webp" fluid/>
                             </p>
                         </Col>
                     </Row>
-                    <Row>
+                    <Row >
                         <Col className="about-col"sm={12}>
                            <h1> Presentations</h1>
                             <br/>
-                            <Row>
-                                <Col>
-                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_9659abae5f834339ad659b16fed79e5c.pdf">M &amp; E Framework</Button>
+                            <Row className="d-block mx-auto w-50">
+                                <Col xs={12}>
+                                <Button className="me-button" href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_9659abae5f834339ad659b16fed79e5c.pdf">M &amp; E Framework</Button>
                                 </Col>
-                                <Col>
-                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_50493217d084460e9712c6f82087f9f5.pdf">Powering Potential M &amp; E</Button>
+                                <Col xs={12}>
+                                <Button  className="me-button" href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_50493217d084460e9712c6f82087f9f5.pdf">Powering Potential M &amp; E</Button>
                                 </Col>
-                                <Col>
-                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_b6cb1ffad7be4cb0bdb8a298259c2a0d.pdf">What Might an Evaluation Look Like?</Button>
+                                <Col xs={12}>
+                                <Button className="me-button"  href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_b6cb1ffad7be4cb0bdb8a298259c2a0d.pdf">What Might an Evaluation Look Like?</Button>
                                 </Col>
                             </Row>
                         </Col>
@@ -72,12 +72,12 @@ export default function MonitoringEvaluation() {
                         <Col className="about-col"sm={12}>
                             <h1> Resources</h1>
                             <br/>
-                            <Row>
-                                <Col>
-                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_6ea09973abe54de0b82624828d1b8d2c.pdf">M &amp; E Framework Resources</Button>
+                            <Row className="d-block mx-auto w-50">
+                                <Col xs={12}>
+                                <Button  className="me-button" href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_6ea09973abe54de0b82624828d1b8d2c.pdf">M &amp; E Framework Resources</Button>
                                 </Col>
-                                <Col>
-                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_ea6584a974f6470bb92e62b966737c27.pdf">M &amp; E Worksheet</Button>
+                                <Col xs={12}>
+                                <Button  className="me-button" href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_ea6584a974f6470bb92e62b966737c27.pdf">M &amp; E Worksheet</Button>
                                 </Col>
                             </Row>
                         </Col>

73e880dd2e933ba2065fed2e8f74c5ca3b581eb0
sean-slaughter <sthompson2822@gmail.com>
2020-11-22 11:46:28 -0600

    Finish Monitoring Evaluation section

--- src/containers/MonitoringEvaluation.js
+++ src/containers/MonitoringEvaluation.js
@@ -1,9 +1,90 @@
 import React from 'react'
+import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 
 export default function MonitoringEvaluation() {
     return (
         <div>
-           Monitoring &amp; Evaluation 
+           <Container style={{marginTop: 50}}>
+           <Row>
+                <Col className="about-col-right" sm={12} md={4}> 
+                
+                    <h1>Monitoring &amp; Evaluation</h1> 
+                    <br/>
+                    <p>
+                        <b>Monitoring:</b>
+                        <br/>
+                         <p className="smaller">Keeping track of what you are doing while you are doing it, so that you can take corrective action, if necessary.
+                        <br/></p>
+                        <br/>
+                        <b>Evaluation:</b> 
+                        <br/>
+                        <p className="smaller">Finding out if you have achieved the effect on your target population that you said you would achieve,
+                        after you have finished implementing the activities</p>
+                    </p>
+                    <h1 style={{marginTop: 50}}>Logic Model</h1>
+                    <Image src="/LogicModel.webp" fluid/>
+                    <b>Inputs:</b> 
+                        <br/>
+                        <p className="smaller">The financial, human, and material resources used for developing, deploying and managing the intervention</p>
+                    <b>Activities:</b> 
+                    <br/>
+                    <p className="smaller">Actions taken or work performed to meet the objectives</p>
+                    <b>Outputs:</b> 
+                        <br/>
+                        <p className="smaller">The products that result from intervention</p>
+                    <b>Outcomes:</b> 
+                    <br/>
+                    <p className="smaller">The results of the program or changes that occur either immediately or some time after activities are completed</p>
+                    <b>Impact:</b> 
+                        <br/>
+                     <p className="smaller">The wider effect of the program on long-term consequences</p>
+
+
+                </Col>
+                <Col className="about-col"sm={12} md={8}>
+                    <Row className="align-items-center justify-content-center">
+                        <Col className="about-col"sm={12}>
+                            <h1>Monitoring &amp; Evaluation Steps</h1> 
+                            <br/>
+                            <p>
+                                <Image className="d-block mx-auto img-fluid w-50" src="/monitoringevaluation.webp" fluid/>
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="about-col"sm={12}>
+                           <h1> Presentations</h1>
+                            <br/>
+                            <Row>
+                                <Col>
+                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_9659abae5f834339ad659b16fed79e5c.pdf">M &amp; E Framework</Button>
+                                </Col>
+                                <Col>
+                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_50493217d084460e9712c6f82087f9f5.pdf">Powering Potential M &amp; E</Button>
+                                </Col>
+                                <Col>
+                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_b6cb1ffad7be4cb0bdb8a298259c2a0d.pdf">What Might an Evaluation Look Like?</Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="about-col"sm={12}>
+                            <h1> Resources</h1>
+                            <br/>
+                            <Row>
+                                <Col>
+                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_6ea09973abe54de0b82624828d1b8d2c.pdf">M &amp; E Framework Resources</Button>
+                                </Col>
+                                <Col>
+                                <Button href="https://150f716c-2597-47c4-b267-f8914e49bec0.filesusr.com/ugd/a6f5ed_ea6584a974f6470bb92e62b966737c27.pdf">M &amp; E Worksheet</Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+           </Container>
         </div>
     )
 }
--- src/containers/Services.js
+++ src/containers/Services.js
@@ -18,7 +18,7 @@ export default function Services() {
                 <Row className="justify-content-center">
                     <Col xs={12} md={4}>
                         <h2>Monitoring &amp; Evaluation</h2>
-                        <p className="services">Monitoring and Evaluation (M &amp; E) is a process that helps improve performance 
+                        <p className="smaller">Monitoring and Evaluation (M &amp; E) is a process that helps improve performance 
                             and achieve desired goals and objectives through improvement in outputs, 
                             outcomes, and impact. Our members are adept in developing M &amp; E framework 
                             including key performance indicators to help improve project/program 
@@ -26,13 +26,13 @@ export default function Services() {
                     </Col>
                     <Col xs={12} md={4}>
                         <h2>Teaching and Training</h2>
-                        <p className="services">Our team is adept is several statistical softwares, such as SPSS, SAS, JMP and R, 
+                        <p className="smaller">Our team is adept is several statistical softwares, such as SPSS, SAS, JMP and R, 
                             and have trained professionals and students in quality data collection, 
                             management and analysis. </p>
                     </Col>
                     <Col xs={12} md={4}>
                         <h2>Project Implementation</h2>
-                        <p className="services">Thorough planning and trained team members are key for a successful project. 
+                        <p className="smaller">Thorough planning and trained team members are key for a successful project. 
                             We have a lot of experience in health-related projects 
                             from a phase of conceptualization, planning, implementation and completion. </p>
                     </Col>
@@ -40,7 +40,7 @@ export default function Services() {
                 <Row className="justify-content-center">
                     <Col xs={12} md={4}>
                         <h2>Survey Designs</h2>
-                        <p className="services">Surveys are one of the important tools that comprise 
+                        <p className="smaller">Surveys are one of the important tools that comprise 
                         of measurement procedures involving asking questions from respondents.
                          Careful and thorough designing of surveys is critical for conducting good research.
                          Our team is extremely competent and has extensive experiencing in 
@@ -48,25 +48,24 @@ export default function Services() {
                     </Col>
                     <Col xs={12} md={4}>
                         <h2>Proposal/Paper Writing</h2>
-                        <p className="services">We have extensive experience in proposal and manuscript writing.
+                        <p className="smaller">We have extensive experience in proposal and manuscript writing.
                          Several manuscripts are published in high-impact factor peer-reviewed journals, 
                          like American Journal of Epidemiology, AIDS, and Journal of Infection. </p>
                     </Col>
                     <Col xs={12} md={4}>
                         <h2>Disease Surveillance</h2>
-                        <p className="services">Surveillance is ongoing and systematic collection, analysis, 
+                        <p className="smaller">Surveillance is ongoing and systematic collection, analysis, 
                         interpretation and dissemination of health information. 
                         Depending upon the available resources and needs, setting up a
                          disease surveillance is helpful in determining the incidence/new 
                          cases of a disease. Our team is experienced in developing disease surveillance in different settings.  </p>
                     </Col>
                     <Col xs={12} md={4}>
-                        <h2>Disease Surveillance</h2>
-                        <p className="services">Surveillance is ongoing and systematic collection, analysis, 
-                        interpretation and dissemination of health information. 
-                        Depending upon the available resources and needs, setting up a
-                         disease surveillance is helpful in determining the incidence/new 
-                         cases of a disease. Our team is experienced in developing disease surveillance in different settings.  </p>
+                        <h2>Artificial Intelligence</h2>
+                        <p className="smaller">Artificial Intelligence (AI) is development of human-like artificial 
+                        intelligence that can perceive, reason, learn or process natural language. 
+                        Employees and customer experiences can be enhanced by creating workflows that utilize AI and data. 
+                        Our team has experience in streamlining and improving processes through in-depth use of AI. </p>
                     </Col>
                 </Row>
             </Container>
